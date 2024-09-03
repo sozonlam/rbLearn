8542394c1f4414c84c8c2fa0b860a1166452ddd2
sozon <sozonlam@gmail.com>
2024-09-03 08:32:29 +0300

    stock_picker

---
+++ .history/stock_picker_20240903082555.rb
@@ -0,0 +1,6 @@
+def stock_picker(prices)
+profit = 0
+best_days = []
+
+
+end  
---
+++ .history/stock_picker_20240903082619.rb
@@ -0,0 +1,18 @@
+def stock_picker(prices)
+  max_profit = 0
+  best_days = []
+
+  prices.each_with_index do |buy_price, buy_day|
+    (buy_day + 1...prices.length).each do |sell_day|
+      sell_price = prices[sell_day]
+      profit = sell_price - buy_price
+
+      if profit > max_profit
+        max_profit = profit
+        best_days = [buy_day, sell_day]
+      end
+    end
+  end
+
+  best_days
+end
